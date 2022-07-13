2bf08cce2c7fbe941b7705cbcc96fdd7dc790431
Dhruv Kelawala <dhruvrk2000@gmail.com>
2022-07-13 18:40:53 +0100

    Revert "feat: add address checksuming"
    This reverts commit 6a960e17abed81648adba6300cfc803a3e3783f5.

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { TradeType } from './constants'
 import invariant from 'tiny-invariant'
+import { validateAndParseAddress } from 'starknet'
 import { CurrencyAmount, ETHER, Percent, Trade } from './entities'
-import { validateAndParseAddress } from './utils'
 
 /**
  * Options for producing the arguments to send call to the router.
